3e072ce88cf6868d80c47c23525f1fa81159e142
ksg-dev <ksg.dev.data@gmail.com>
2024-06-27 16:19:45 -0500

    grab article link and upvotes

--- scrape.py
+++ scrape.py
@@ -7,10 +7,17 @@ response = requests.get("https://news.ycombinator.com/news")
 yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
-article = soup.find(name="span", class_="titleline")
+article = soup.find(name="span", class_="titleline").find("a")
 
 article_title = article.getText()
+article_link = article.get("href")
+article_score = soup.find(name="span", class_="score").getText()
+# print(article_title)
+
+print(article)
 print(article_title)
+print(article_link)
+print(article_score)
 
 
 
