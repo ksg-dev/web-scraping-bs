e01d76152ca603abdd61a35d63aedbb55be8a883
ksg-dev <ksg.dev.data@gmail.com>
2024-06-27 16:08:12 -0500

    grab article title

--- scrape.py
+++ scrape.py
@@ -4,4 +4,14 @@ import requests
 # Use requests to get data from URL
 response = requests.get("https://news.ycombinator.com/news")
 
-print(response.text)
+yc_webpage = response.text
+
+soup = BeautifulSoup(yc_webpage, "html.parser")
+article = soup.find(name="span", class_="titleline")
+
+article_title = article.getText()
+print(article_title)
+
+
+
+
