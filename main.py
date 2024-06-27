e03430d2646be628b0467cf285545d2098b2631c
ksg-dev <ksg.dev.data@gmail.com>
2024-06-27 15:50:54 -0500

    starting practice

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ with open("website.html") as file:
 
 # first what text, then what parser
 soup = BeautifulSoup(contents, "html.parser")
+
+# can tap into tags like python objects
+# print(soup.title.string)
+
+# Using findall to find all tags not just first occurrence
+all_anchor_tags = soup.find_all(name="a")
+
+
+# for tag in all_anchor_tags:
+    # print(tag.get("href"))
+
+# find specific tag by id
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+
+# find specific things by class
+section_head = soup.find(name="h3", class_="heading")
+
+# find by css selector - anchor sitting inside p tag
+company_url = soup.select_one(selector="p a")
+print(company_url)
+
