d3027972f030e3a2fc91a197b2e212ecda5f7b37
ksg-dev <ksg.dev.data@gmail.com>
2024-06-27 16:31:11 -0500

    save titles, links, and votes to list

--- scrape.py
+++ scrape.py
@@ -7,17 +7,23 @@ response = requests.get("https://news.ycombinator.com/news")
 yc_webpage = response.text
 
 soup = BeautifulSoup(yc_webpage, "html.parser")
-article = soup.find(name="span", class_="titleline").find("a")
+articles = soup.find_all(name="span", class_="titleline")
+article_texts = []
+article_links = []
+# print(f"articles: {articles}")
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.find("a").get("href")
+    article_links.append(link)
+    # print(f"article_link: {link}")
 
-article_title = article.getText()
-article_link = article.get("href")
-article_score = soup.find(name="span", class_="score").getText()
-# print(article_title)
+article_scores = [score.getText() for score in soup.find_all(name="span", class_="score")]
 
-print(article)
-print(article_title)
-print(article_link)
-print(article_score)
+
+print(article_texts)
+print(article_links)
+print(article_scores)
 
 
 
